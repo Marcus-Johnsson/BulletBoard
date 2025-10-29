c9d37bd081310110c59fd1850e2b57296c281d0e
Glenn Wahlbeck <glenn.wahlbeck@campusnykoping.se>
2025-10-29 15:47:27 +0100

    Add create note endpoint

--- BulletBoard/EndPoints/Notes/CreateNotes.cs
+++ BulletBoard/EndPoints/Notes/CreateNotes.cs
@@ -8,18 +8,35 @@ namespace BulletBoard.EndPoints.Notes;
 public class CreateNotes
 {
     public static void MapEndpoint(IEndpointRouteBuilder app) => app
-        .MapGet("/createnotes", Handle)
-        .Produces<CreateNotes>(StatusCodes.Status201Created)
+        .MapPost("/createnotes", Handle)
+        .Produces<Response>(StatusCodes.Status201Created)
         .Produces(StatusCodes.Status400BadRequest)
         .Produces(StatusCodes.Status401Unauthorized);
     
-    private record Request(Note note);
-    
-    private class Response{}
+    private record Request(
+        string Title,
+        string Description
+        );
 
-    private static async Task<Response> Handle(
-    [FromBody]  Request request)
+    
+
+    private static async Task<IResult> Handle(
+    [FromBody]  Request request,
+    [FromServices] BulletDbContext dbContext
+    )
+
     {
+        var newNote = new Note
+        {
+            Title = request.Title,
+            Description = request.Description,
+            CreatedAt = DateTime.Now
+        };
+        dbContext.Note.Add(newNote);
+        await dbContext.SaveChangesAsync();
         
+        var response = new Response(newNote.Id, newNote.Title);
+        return Results.Ok(response);
     }
+    private record Response(int Id, string Title);//private record Response(int Id, string SupplierName);
 }
